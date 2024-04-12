66c14cdca3f2e22609644703d5ba8e50e1ae8102
TrodriTen <t.rodriguezp@uniandes.edu.co>
2024-04-12 14:59:49 -0500

    Alter urls

--- clientes/urls.py
+++ clientes/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import createClienteR, clientesList, clienteByDocument, deleteClient, updateClient
+from .views import createClient, clientesList, clienteByDocument, deleteClient, updateClient
 
 urlpatterns = [
     path('clientes/', clientesList),
-    path('clientes/createCliente/', createClienteR),  
+    path('clientes/createCliente/', createClient),  
     path('clientes/<int:document>/', clienteByDocument),  
     path('clientes/delete/<int:document>/', deleteClient),  
     path('clientes/updateClient/<int:document>/', updateClient), 
