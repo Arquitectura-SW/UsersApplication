4b4fd4ce824aea04856ab51356bf4b1fb73a55f5
TrodriTen <t.rodriguezp@uniandes.edu.co>
2024-04-12 12:54:33 -0500

    Cliente errors

--- clientes/views.py
+++ clientes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib import messages
-from .forms import clienteForm
+from .forms import ClienteForm
 from .serializer import ClientSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
