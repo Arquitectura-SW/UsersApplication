f1d81905e65930d08f7ac77e360c0b00092c38fa
SofRozo <as.rozo@uniandes.edu.co>
2024-04-12 14:31:44 -0500

    changes

--- clientes/views.py
+++ clientes/views.py
@@ -33,6 +33,7 @@ def clienteByDocument(request, document):
 def createClient(request):
     if request.method == 'POST':
         try:
+            print("Datos recibidos:", request.data)
             cliente = createCliente(request.data)
             serializer = ClientSerializer(cliente)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
