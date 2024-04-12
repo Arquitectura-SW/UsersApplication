f42385539c6b90665ee4d5b5057530615b3dab69
TrodriTen <t.rodriguezp@uniandes.edu.co>
2024-04-12 14:57:44 -0500

    Alter Apps Isntalled

--- UsersApplication/settings.py
+++ UsersApplication/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'usuarios',
     'clientes',
-    'administradores',
     'rest_framework',
 ]
 
