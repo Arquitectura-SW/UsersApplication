5d6d98a7e8c6fcdf0b61732ecb7d414fad49dca3
TrodriTen <t.rodriguezp@uniandes.edu.co>
2024-04-12 13:39:38 -0500

    Cliente errors

--- clientes/logic/logic_clientes.py
+++ clientes/logic/logic_clientes.py
@@ -46,45 +46,13 @@ def deleteClienteByDocumento(document):
     
 def updateClienteByDocumento(document, formCliente):
     try:
-        is_valid, message = validate_client_data(formCliente)
-        if is_valid:
             user = formCliente.save()
             user.save()
             return Cliente.objects.get(document=document)
     except:
-        is_valid, message = validate_client_data(formCliente)
-        if not is_valid:
             raise Exception({'detail': message}, 400)
     
     
-# Auxiliary function 
-def validate_client_data(client_data):
-    id_number = client_data.get('document')
-    birth_date_str = client_data.get('birthdate')
-    message = ''
-    is_valid = True
-    
-    if id_number:
-        client = getClienteByDocumento(id_number)
-        if client is not None:
-            message += "The client with the provided document number already exists in the system. "
-            is_valid = False
-
-    if birth_date_str:
-        birth_date = datetime.strptime(birth_date_str, '%Y-%m-%d').date()
-        today = datetime.now().date()
-        age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
-        if age < 18:
-            message += "The client must be of legal age to register in the system. "
-            is_valid = False
-
-    if id_number:
-        if len(id_number) != 10:
-            message += "The document number (cedula) must have 10 digits."
-            is_valid = False
-
-    return is_valid, message if message else None
-
     
     
     
